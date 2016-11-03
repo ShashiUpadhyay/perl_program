a257ee04b4d2c54a9fa1f51f4b3ac8382726b92f
Shashi Upadhyay <supadhy2@binghamton.edu>
2016-11-03 04:53:03 -0400

    commit

---
+++ match.pl
@@ -0,0 +1,39 @@
+
+my $filename = "input.txt";
+
+if(-e $filename && -r $filename){
+    
+    open FILE, "$filename" or die $!;
+    my $word;
+    print("\n");
+    while(<FILE>){
+        $word = "$_";
+        chomp($word);
+        # string that contains hi
+        if($word=~m/hi/){
+            print "$word contains \"hi\" \n";
+        }
+        
+        # string that contains exactly one vowel characters
+        if($word=~m/^[^aeiou]*[aeiou]{1}[^aeiou]*$/){
+            print "$word contains exactly one vowel character \n";
+        }
+        
+        # string that contains two or more 'l'
+        $count= $word=~tr/l/l/;
+        if($count>=2){
+            print "$word contains two or more \'l\' \n";
+        }
+        
+        # string that begin with letter "h" and end with "t"
+        if($word=~/^h/ && $word=~/t$/){
+            print "$word begin with letter h and ends with letter t \n";
+        }
+        
+    }#end of while loop
+    
+    close(FILE);
+    
+}else{
+    print "$filename : file does not exist\n";
+}
